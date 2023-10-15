af6e4c67caa3e34a953093e5713d0ac8d7661c5d
hoolywant <sir.saltickow@yandex.ru>
2023-10-16 00:51:07 +0400

    fixes

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,6 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('user/<int:pk>', UserRetrieveApi.as_view(), name='пользователь')
 ]
